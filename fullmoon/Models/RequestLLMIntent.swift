05a4b9ed97972414ae4be37bab72df5469c820bc
Jordan Singer <singer.jordan@gmail.com>
2024-12-14 13:46:31 -0500

    working

--- fullmoon/Models/RequestLLMIntent.swift
+++ fullmoon/Models/RequestLLMIntent.swift
@@ -4,32 +4,36 @@ import SwiftData
 
 @available(iOS 16.0, macOS 13.0, *)
 struct RequestLLMIntent: AppIntent {
-    static var title: LocalizedStringResource = "Request LLM"
-    static var description: LocalizedStringResource = "Send a prompt to the LLM and get a response"
-    
-    @Parameter(title: "Prompt")
+    static var title: LocalizedStringResource = "new chat"
+    static var description: LocalizedStringResource = "start a new chat"
+
+    @Parameter(title: "message", requestValueDialog: IntentDialog("new chat"))
     var prompt: String
     
     static var parameterSummary: some ParameterSummary {
-        Summary("Request LLM with \(\.$prompt)")
+        Summary("new chat with \(\.$prompt)")
     }
-    
+        
     @MainActor
-    func perform() async throws -> some ReturnsValue<String> {
+    func perform() async throws -> some IntentResult & ReturnsValue<String> & ProvidesDialog  {
         let llm = LLMEvaluator()
         let appManager = AppManager()
         
         if let modelName = appManager.currentModelName {
             _ = try? await llm.load(modelName: modelName)
+            
             let thread = Thread()
+            let message = Message(role: .user, content: prompt, thread: thread)
+            thread.messages.append(message)
             let output = await llm.generate(modelName: modelName, thread: thread, systemPrompt: appManager.systemPrompt)
-            return .result(value: output)
+            let responseMessage = Message(role: .assistant, content: output, thread: thread)
+            thread.messages.append(responseMessage)
+            return .result(value: output, dialog: "\(output)")
         } else {
-            return .result(value: "No model is currently selected. Please open the app and select a model first.")
+            let error = "no model is currently selected. open the app and select a model first."
+            return .result(value: error, dialog: "\(error)")
         }
     }
-}
-
-extension RequestLLMIntent {
+    
     static var openAppWhenRun: Bool = false
-} 
+}
