0eb7704d4797f9f828a7a46b094fd717d59d6ad1
Xavier Normant <30378214+atlou@users.noreply.github.com>
2024-12-14 17:36:46 -0500

    Implement infinite prompt

--- fullmoon/Models/RequestLLMIntent.swift
+++ fullmoon/Models/RequestLLMIntent.swift
@@ -1,6 +1,6 @@
 import AppIntents
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @available(iOS 16.0, macOS 13.0, *)
 struct RequestLLMIntent: AppIntent {
@@ -9,31 +9,36 @@ struct RequestLLMIntent: AppIntent {
 
     @Parameter(title: "message", requestValueDialog: IntentDialog("new chat"))
     var prompt: String
-    
+
     static var parameterSummary: some ParameterSummary {
         Summary("new chat with \(\.$prompt)")
     }
-        
+    
+    let thread = Thread() // create a new thread for this intent
+
     @MainActor
-    func perform() async throws -> some IntentResult & ReturnsValue<String> & ProvidesDialog  {
+    func perform() async throws -> some IntentResult & ProvidesDialog {
         let llm = LLMEvaluator()
         let appManager = AppManager()
-        
+
         if let modelName = appManager.currentModelName {
             _ = try? await llm.load(modelName: modelName)
-            
-            let thread = Thread()
-            let message = Message(role: .user, content: prompt, thread: thread)
+
+            let message = Message(role: .user, content: prompt ?? "test", thread: thread)
             thread.messages.append(message)
             let output = await llm.generate(modelName: modelName, thread: thread, systemPrompt: appManager.systemPrompt)
             let responseMessage = Message(role: .assistant, content: output, thread: thread)
             thread.messages.append(responseMessage)
+
+            throw $prompt.requestValue("\(output)") // re-prompt infinitely until user cancels
+
             return .result(value: output, dialog: "\(output)")
-        } else {
+        }
+        else {
             let error = "no model is currently selected. open the app and select a model first."
             return .result(value: error, dialog: "\(error)")
         }
     }
-    
+
     static var openAppWhenRun: Bool = false
 }
