417349d0a26a07ff45a0d3ef1e4d2b1fa9376cf5
Jordan Singer <singer.jordan@gmail.com>
2024-12-16 11:11:55 -0500

    siri shortcut for new chat (#38)
    * functional intent
    
    * working
    
    * Implement infinite prompt
    
    * Remove dialogs + Add return value
    
    * Add continuous toggle
    
    * Add output dialog if chat is not continuous
    
    * string changes
    
    ---------
    
    Co-authored-by: Xavier Normant <30378214+atlou@users.noreply.github.com>

---
+++ fullmoon/Models/RequestLLMIntent.swift
@@ -0,0 +1,64 @@
+import AppIntents
+import SwiftData
+import SwiftUI
+
+@available(iOS 16.0, macOS 13.0, *)
+struct RequestLLMIntent: AppIntent {
+    static var title: LocalizedStringResource = "new chat"
+    static var description: LocalizedStringResource = "start a new chat"
+    
+    @Parameter(title: "continuous chat", default: true)
+    var continuous: Bool
+    
+    @Parameter(title: "message", requestValueDialog: IntentDialog("chat"))
+    var prompt: String
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("new chat with \(\.$prompt)") {
+            // shortcuts additional parameters
+            \.$continuous
+        }
+    }
+    
+    let thread = Thread() // create a new thread for this intent
+
+    @MainActor
+    func perform() async throws -> some IntentResult & ReturnsValue<String> & ProvidesDialog {
+        let llm = LLMEvaluator()
+        let appManager = AppManager()
+        
+        if prompt.isEmpty {
+            if let output = thread.messages.last?.content {
+                return .result(value: output, dialog: "continue chatting in the app") // if prompt is empty and this is not the first message, return the result
+            } else {
+                throw $prompt.requestValue("chat") // re-prompt
+            }
+        }
+
+        if let modelName = appManager.currentModelName {
+            _ = try? await llm.load(modelName: modelName)
+            
+            let message = Message(role: .user, content: prompt, thread: thread)
+            thread.messages.append(message)
+            let output = await llm.generate(modelName: modelName, thread: thread, systemPrompt: appManager.systemPrompt)
+            let responseMessage = Message(role: .assistant, content: output, thread: thread)
+            thread.messages.append(responseMessage)
+
+            if continuous {
+                throw $prompt.requestValue("\(output)") // re-prompt infinitely until user cancels
+            }
+            
+            if continuous {
+                return .result(value: output, dialog: "continue chatting in the app")
+            }
+            
+            return .result(value: output, dialog: "\(output)")
+        }
+        else {
+            let error = "no model is currently selected. open the app and select a model first."
+            return .result(value: error, dialog: "\(error)")
+        }
+    }
+
+    static var openAppWhenRun: Bool = false
+}
