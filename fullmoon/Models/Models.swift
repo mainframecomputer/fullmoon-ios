1a2c2c98b3316a4dbe9dcb44afdabd9d4885fc58
Xavier Normant <30378214+atlou@users.noreply.github.com>
2025-01-27 16:05:43 -0500

    Fix Deepseek-R1 chat template issue (#84)
    * add deepseek-r1-distill-qwen-1.5b
    
    * Update Models.swift
    
    * format thinking
    
    * 4bit model, styling
    
    * add thinking collapsing
    
    * add chevron
    
    * borderless chevron
    
    * fix mac bugs
    
    * tmp think hack
    
    * Update Models.swift
    
    * disable haptics for thinking + model type enum
    
    * remove user injected <think>
    
    * pull main
    
    * remove orig
    
    * Fix chat template issue
    
    * Add comments
    
    * Add 8bit model
    
    ---------
    
    Co-authored-by: Jordan Singer <singer.jordan@gmail.com>

--- fullmoon/Models/Models.swift
+++ fullmoon/Models/Models.swift
@@ -5,17 +5,18 @@
 //  Created by Jordan Singer on 10/4/24.
 //
 
-import MLXLMCommon
 import Foundation
+import MLXLMCommon
 
 public extension ModelConfiguration {
     enum ModelType {
         case regular, reasoning
     }
-    
+
     var modelType: ModelType {
         switch self {
         case .deepseek_r1_distill_qwen_1_5b_4bit: .reasoning
+        case .deepseek_r1_distill_qwen_1_5b_8bit: .reasoning
         default: .regular
         }
     }
@@ -25,29 +26,34 @@ extension ModelConfiguration: @retroactive Equatable {
     public static func == (lhs: MLXLMCommon.ModelConfiguration, rhs: MLXLMCommon.ModelConfiguration) -> Bool {
         return lhs.name == rhs.name
     }
-    
+
     public static let llama_3_2_1B_4bit = ModelConfiguration(
         id: "mlx-community/Llama-3.2-1B-Instruct-4bit"
     )
-    
+
     public static let llama_3_2_3b_4bit = ModelConfiguration(
         id: "mlx-community/Llama-3.2-3B-Instruct-4bit"
     )
-    
+
     public static let deepseek_r1_distill_qwen_1_5b_4bit = ModelConfiguration(
         id: "mlx-community/DeepSeek-R1-Distill-Qwen-1.5B-4bit"
     )
     
+    public static let deepseek_r1_distill_qwen_1_5b_8bit = ModelConfiguration(
+        id: "mlx-community/DeepSeek-R1-Distill-Qwen-1.5B-8bit"
+    )
+
     public static var availableModels: [ModelConfiguration] = [
         llama_3_2_1B_4bit,
         llama_3_2_3b_4bit,
-        deepseek_r1_distill_qwen_1_5b_4bit
+        deepseek_r1_distill_qwen_1_5b_4bit,
+        deepseek_r1_distill_qwen_1_5b_8bit
     ]
-    
+
     public static var defaultModel: ModelConfiguration {
         llama_3_2_1B_4bit
     }
-    
+
     public static func getModelByName(_ name: String) -> ModelConfiguration? {
         if let model = availableModels.first(where: { $0.name == name }) {
             return model
@@ -55,34 +61,46 @@ extension ModelConfiguration: @retroactive Equatable {
             return nil
         }
     }
-    
+
     func getPromptHistory(thread: Thread, systemPrompt: String) -> [[String: String]] {
         var history: [[String: String]] = []
-        
+
         // system prompt
         history.append([
             "role": "system",
-            "content": systemPrompt
+            "content": systemPrompt,
         ])
-        
+
         // messages
         for message in thread.sortedMessages {
             let role = message.role.rawValue
             history.append([
                 "role": role,
-                "content": message.content
+                "content": formatForTokenizer(message.content), // Remove think tags and add a space before each message to fix the Jinja chat template issue.
             ])
         }
-        
+
         return history
     }
-    
+
+    // TODO: Remove this function when Jinja gets updated
+    func formatForTokenizer(_ message: String) -> String {
+        if self.modelType == .reasoning {
+            return " " + message
+                .replacingOccurrences(of: "<think>", with: "")
+                .replacingOccurrences(of: "</think>", with: "")
+        }
+        
+        return message
+    }
+
     /// Returns the model's approximate size, in GB.
     public var modelSize: Decimal? {
         switch self {
         case .llama_3_2_1B_4bit: return 0.7
         case .llama_3_2_3b_4bit: return 1.8
         case .deepseek_r1_distill_qwen_1_5b_4bit: return 1.0
+        case .deepseek_r1_distill_qwen_1_5b_8bit: return 1.9
         default: return nil
         }
     }
