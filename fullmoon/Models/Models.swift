059c082fcfc070488af7d7f7c9c31bfd57caf472
Xavier Normant <30378214+atlou@users.noreply.github.com>
2025-01-27 14:57:22 -0500

    Fix chat template issue

--- fullmoon/Models/Models.swift
+++ fullmoon/Models/Models.swift
@@ -5,14 +5,14 @@
 //  Created by Jordan Singer on 10/4/24.
 //
 
-import MLXLMCommon
 import Foundation
+import MLXLMCommon
 
 public extension ModelConfiguration {
     enum ModelType {
         case regular, reasoning
     }
-    
+
     var modelType: ModelType {
         switch self {
         case .deepseek_r1_distill_qwen_1_5b_4bit: .reasoning
@@ -25,29 +25,29 @@ extension ModelConfiguration: @retroactive Equatable {
     public static func == (lhs: MLXLMCommon.ModelConfiguration, rhs: MLXLMCommon.ModelConfiguration) -> Bool {
         return lhs.name == rhs.name
     }
-    
+
     public static let llama_3_2_1B_4bit = ModelConfiguration(
         id: "mlx-community/Llama-3.2-1B-Instruct-4bit"
     )
-    
+
     public static let llama_3_2_3b_4bit = ModelConfiguration(
         id: "mlx-community/Llama-3.2-3B-Instruct-4bit"
     )
-    
+
     public static let deepseek_r1_distill_qwen_1_5b_4bit = ModelConfiguration(
         id: "mlx-community/DeepSeek-R1-Distill-Qwen-1.5B-4bit"
     )
-    
+
     public static var availableModels: [ModelConfiguration] = [
         llama_3_2_1B_4bit,
         llama_3_2_3b_4bit,
-        deepseek_r1_distill_qwen_1_5b_4bit
+        deepseek_r1_distill_qwen_1_5b_4bit,
     ]
-    
+
     public static var defaultModel: ModelConfiguration {
         llama_3_2_1B_4bit
     }
-    
+
     public static func getModelByName(_ name: String) -> ModelConfiguration? {
         if let model = availableModels.first(where: { $0.name == name }) {
             return model
@@ -55,28 +55,34 @@ extension ModelConfiguration: @retroactive Equatable {
             return nil
         }
     }
-    
+
     func getPromptHistory(thread: Thread, systemPrompt: String) -> [[String: String]] {
         var history: [[String: String]] = []
-        
+
         // system prompt
         history.append([
             "role": "system",
-            "content": systemPrompt
+            "content": systemPrompt,
         ])
-        
+
         // messages
         for message in thread.sortedMessages {
             let role = message.role.rawValue
             history.append([
                 "role": role,
-                "content": message.content
+                "content": " " + removeThinkTags(message.content),
             ])
         }
-        
+
         return history
     }
-    
+
+    func removeThinkTags(_ message: String) -> String {
+        return message
+            .replacingOccurrences(of: "<think>", with: "")
+            .replacingOccurrences(of: "</think>", with: "")
+    }
+
     /// Returns the model's approximate size, in GB.
     public var modelSize: Decimal? {
         switch self {
